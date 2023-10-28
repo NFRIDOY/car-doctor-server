a985d7117dba702d1dae1276035ba8a88e1b3a0f
NF RIDOY <nfridoy@gmail.com>
2023-10-28 17:15:48 +0600

    jwt + cookie parser install

--- index.js
+++ index.js
@@ -2,13 +2,21 @@ const express = require("express");
 const cors = require("cors");
 require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken'); // npm install jsonwebtoken
+const cookie = require('cookie-parser') // npm install cookie-parser
 const port = process.env.PORT || 5000;
 const app = express();
 
-// Middlewere
+// Middleware
 
 app.use(cors());
 app.use(express.json());
+
+// Middleware Custom
+
+const verify = () => {
+    
+}
 
 // Mongo Driver
 
@@ -41,47 +49,74 @@ async function run() {
         const database = client.db("carDoctorDB");
         const servicesCollection = database.collection("services");
 
+        // JWT
+        app.post('/jwt', async (req, res) => {
+            try {
+                const body = req.body;
+
+                const token = jwt.sign(body, process.env.ACCESS_TOKEN_SECRET, {
+                    expiresIn: '1h'
+                })
+
+                res.cookie("token", token, {
+
+                })
+                // res.send(body)
+            } catch (error) {
+                console.log(error)
+            }
+        })
+
         // CRUD Operations
 
         //Get All services
         app.get('/services', async (req, res) => {
-            const query = {};
-            const options = {
-                // Sort returned documents in ascending order by title (A->Z)
-                sort: { service_id: 1 },
-                // Include only the `title` and `imdb` fields in each returned document
-                // projection: { _id: 1, title: 1, service_id: 1, price:1},
-            };
-            // Execute query 
-            const cursor = servicesCollection.find(query, options);
-            const services = await cursor.toArray()
-            // console.log(services)
-            res.send(services)
-            // Print a message if no documents were found
-            // if ((await servicesCollection.countDocuments()) === 0) {
-            //     console.log("No documents found!");
-            // }
-            // else {
-            //     console.log("Founded")
-            // }
+            try {
+                const query = {};
+                const options = {
+                    // Sort returned documents in ascending order by title (A->Z)
+                    sort: { service_id: 1 },
+                    // Include only the `title` and `imdb` fields in each returned document
+                    // projection: { _id: 1, title: 1, service_id: 1, price:1},
+                };
+                // Execute query 
+                const cursor = servicesCollection.find(query, options);
+                const services = await cursor.toArray()
+                // console.log(services)
+                res.send(services)
+                // Print a message if no documents were found
+                // if ((await servicesCollection.countDocuments()) === 0) {
+                //     console.log("No documents found!");
+                // }
+                // else {
+                //     console.log("Founded")
+                // }
 
-            // // Print returned documents
-            // for await (const service of services) {
-            //     console.dir(service);
-            // }
-            // await services.forEach((service) => {
-            //     console.log(service)
-            // })
-            
+                // // Print returned documents
+                // for await (const service of services) {
+                //     console.dir(service);
+                // }
+                // await services.forEach((service) => {
+                //     console.log(service)
+                // })
+            }
+            catch (error) {
+                console.log(error)
+            }
+
         })
 
-        // Get Onle services // services Details
+        // Get One services // services Details
 
         app.get('/services/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = {_id : new ObjectId(id)}
-            const cursor = await servicesCollection.findOne(query)
-            res.send(cursor)
+            try {
+                const id = req.params.id;
+                const query = { _id: new ObjectId(id) }
+                const cursor = await servicesCollection.findOne(query)
+                res.send(cursor)
+            } catch (error) {
+                console.log(error)
+            }
         })
 
 
